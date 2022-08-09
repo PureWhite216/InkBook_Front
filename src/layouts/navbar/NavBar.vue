a6bb538427b57f409a4a6a855c9aa8b2043abf81
zjy <1565010390@qq.com>
2022-08-10 01:22:47 +0800

    search

--- src/layouts/navbar/NavBar.vue
+++ src/layouts/navbar/NavBar.vue
@@ -1,7 +1,46 @@
 <template>
   <div class="vaw-nav-bar-wrapper">
     <div style="flex: 1">
-      <el-input v-model="input" placeholder="Search" size="mini" style="width: 200px; margin-left: 20px" />
+      <el-popover
+        v-model="searchProject_visible"
+        placement="top"
+        width="450"
+        height="400"
+      >
+        <p align="center">项目</p>
+        <el-table v-loading="loading" :data="searchProjectList" height="400">
+          <el-table-column width="80" property="project_name" label="名称" align="center" />
+          <el-table-column width="140" property="project_info" label="简介" align="center" />
+          <el-table-column width="200" label="操作" align="center">
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                type="primary"
+                :underline="false"
+                text-align="left"
+                icon="el-icon-view"
+                @click="viewSearchWord(scope.row)"
+              >查看</el-button>
+              <el-button
+                size="mini"
+                type="warning"
+                :underline="false"
+                text-align="left"
+                icon="el-icon-star-on"
+                @click="Like(scope.row)"
+              >收藏</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- <el-button
+          slot="reference"
+          style="margin-inline:10px; color: white; background: #2c2c2c"
+          size="mini"
+          icon="el-icon-search"
+        >搜索
+        </el-button> -->
+        <el-input slot="reference" v-model="keyword_project" placeholder="搜索项目" prefix-icon="el-icon-search" size="mini" style="width: 200px; margin-left: 20px" @input="searchProject_visible = true,getSearchProjectList()" />
+      </el-popover>
     </div>
     <div>
       <el-popover
@@ -50,6 +89,7 @@
           icon="el-icon-search"
         >搜索
         </el-button>
+        <!-- <el-input slot="reference" v-model="keyword" placeholder="Search" prefix-icon="el-icon-search" size="mini" style="width: 200px; margin-left: 20px" @input="search_visible = true,getSearchDocList()" /> -->
       </el-popover>
     </div>
     <!--    <div class="right-wrapper">-->
@@ -71,10 +111,20 @@ export default {
   data() {
     return {
       state: store.state,
+      searchProject_visible: false,
       search_visible: false,
       loading: false,
       searchDocList: [],
+      searchProjectList: [],
+      deprecatedList: [],
       keyword: null,
+      keyword_project: null,
+      form_getSearchProjectList: {
+        token: getters.getToken(state),
+        user_id: getters.getUserId(state),
+        team_id: localStorage.getItem('team_id'),
+        keyword: null
+      },
       form_getSearchWordList: {
         token: getters.getToken(state),
         user_id: getters.getUserId(state),
@@ -96,6 +146,40 @@ export default {
     }
   },
   methods: {
+    getSearchProjectList() {
+      this.loading = true
+      this.searchProjectList = []
+      this.deprecatedList = []
+      this.$axios.get('/project/search', {
+              params: {
+                token: getters.getToken(state),
+                user_id: getters.getUserId(state),
+                team_id: localStorage.getItem('team_id'),
+                keyword: this.keyword_project
+              }
+            })
+        .then((res) => {
+          if (res.data.success) {
+            for (let i = 0; i < res.data.data.length; i++) {
+              const projects = {
+                project_id: 0,
+                team_id: 0,
+                project_name: '',
+                project_info: ''
+              }
+              projects.project_id = res.data.data[i].project_id
+              projects.team_id = res.data.data[i].team_id
+              projects.project_name = res.data.data[i].project_name
+              projects.project_info = res.data.data[i].project_info
+              if (!res.data.data[i].deprecated) { this.searchProjectList.push(projects) } else { this.deprecatedList.push(projects) }
+              // this.$message.success(res.data.message)
+            }
+          } else {
+             // this.$message.error(res.data.message)
+          }
+           this.loading = false
+         })
+    },
     viewSearchWord(item) {
       this.form_viewSearchWord.word_id = item.word_id
       this.$axios.post('/worddocx/user_look_search_word_content', qs.stringify(this.form_viewSearchWord))
@@ -123,7 +207,7 @@ export default {
         })
     },
     getSearchDocList() {
-      // this.loading = true
+      this.loading = true
       this.searchDocList = []
       this.$axios.get('/doc/searchDoc', {
               params: {
@@ -169,7 +253,8 @@ export default {
               } else {
                 this.$message.error(res.data.message)
               }
-            })
+        })
+        this.loading = false
     },
     getSearchWordList() {
       this.loading = true
